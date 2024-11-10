961ededf46c9fc668607df55ee1ad19be5f8fc2b
xsefx1925 <xsefx1925@gmail.ru>
2024-11-10 14:28:33 +0300

    copy text done

--- WinAPI/WinAPI/resource.h
+++ WinAPI/WinAPI/resource.h
@@ -9,6 +9,7 @@
 #define IDC_STATIC_PASSWORD             1003
 #define IDC_EDIT2                       1004
 #define IDC_EDIT_PASSWORD               1004
+#define IDC_BUTTON_COPY                 1005
 
 // Next default values for new objects
 // 
@@ -16,7 +17,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
